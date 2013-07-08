62b6b737db5dd635c08558e5e6728bee3af9c10d
wlandau <will.landau@gmail.com>
2013-07-07 19:53:12 -0500

    "."

--- include/constants.h
+++ include/constants.h
@@ -7,6 +7,7 @@
 #define MAXROW 16384
 #define NUM_TF "%0.3f"
 #define NUM_TMIN FLT_MIN
+#define M_PI 3.1415926535897932384626433832795028841971693993751058
 
 typedef int count_t;
 typedef float num_t;
