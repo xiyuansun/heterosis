b064e64f6c8b343355dec2ce0c3c4ee8ebd538c3
wlandau <will.landau@gmail.com>
2013-07-07 20:08:02 -0500

    "."

--- src/config.c
+++ src/config.c
@@ -8,7 +8,7 @@
 #include <string.h>
 
 Config *config(int argc, char **argv){
-  Config *cfg = malloc(sizeof(Config));
+  Config *cfg = calloc(1, sizeof(Config));
 
   cfg->dataFile = malloc(BUF * sizeof(char));
   cfg->groupFile = malloc(BUF * sizeof(char));
