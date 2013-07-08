5d8ffe36373833b117ca0a4b880e92dedf651f95
wlandau <will.landau@gmail.com>
2013-07-08 11:08:56 -0500

    "."

--- src/gpu/runiform.c
+++
@@ -1,8 +0,0 @@
-#include <constants.h>
-#include <math.h>
-#include <stdlib.h>
-
-num_t runiform(num_t lb, num_t ub){
-  num_t u = ((num_t) rand()) / ((num_t) RAND_MAX);
-  return (ub - lb) * u + lb;
-}
