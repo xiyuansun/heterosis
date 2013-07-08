768d86e0c288c491d86e4df1d403d15918a38d48
wlandau <will.landau@gmail.com>
2013-07-08 16:07:12 -0500

    "."

--- src/gpu/oneChain.cu
+++ src/gpu/oneChain.cu
@@ -9,14 +9,27 @@ void oneChain(int argc, char **argv){
   Config *cfg = config(argc, argv);  
   Chain *a = newChain(cfg);
   
+  printf("hi1\n");
+  
   if(a == NULL){
     freeConfig(cfg);
     exit(EXIT_FAILURE);
   }
+    printf("hi2\n");
   
   runChain(a, cfg);
+
+  printf("hi3\n");
+
   summarizeChain(a, cfg);  
 
+  printf("hi4\n");
+
   freeChain(a, cfg);
+  
+    printf("hi5\n");
+  
   freeConfig(cfg);
+  
+    printf("hi6\n");
 }
