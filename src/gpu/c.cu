5dfa178edeeb8d4ebbbc489646889894f502c084
wlandau <will.landau@gmail.com>
2013-07-10 09:44:12 -0500

    "."

--- src/gpu/c.cu
+++ src/gpu/c.cu
@@ -89,7 +89,7 @@ __global__ void sampleC_kernel3(Chain *a){ /* kernel <<<1, 1>>> */
 
 __host__ void sampleC(Chain *host_a, Chain *dev_a, Config *cfg){ /* host */
   int n, N = cfg->N;
-  int nthreads = (N < cfg->nthreads ? N : cfg->nthreads);
+  int nthreads = (N < MAXTHREADS ? N : MAXTHREADS);
   int nblocks = ceil(cfg->N / NTHREADS);
   
   sampleC_kernel1<<<nblocks, nthreads>>>(dev_a);
