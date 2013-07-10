2b05f49f8564af66c97805f88331acc589020e80
wlandau <will.landau@gmail.com>
2013-07-09 20:04:23 -0500

    "."

--- src/gpu/newChain.cu
+++ src/gpu/newChain.cu
@@ -127,7 +127,7 @@ __host__ Chain *newChain(Config *cfg){ /* host */
   CUDA_CALL(cudaMemcpy(&(a->allParmsFlag), &(cfg->allParmsFlag), sizeof(int), cudaMemcpyHostToDevice));  
   
   
-  CUDA_CALL(cudaMalloc((void **)&(a->grp), sizeof(int))); printf("---\n", n);
+  CUDA_CALL(cudaMalloc((void **)&(a->grp), sizeof(int)));
   for(n = 0; n < cfg->N; ++n){
     printf("%d", n);
   
