86404cdd14d4ba715b912733ce5bc606845b42e3
wlandau <will.landau@gmail.com>
2013-07-11 10:35:22 -0500

    "."

--- src/gpu/runChain.cu
+++ src/gpu/runChain.cu
@@ -11,20 +11,20 @@ void runChain(Chain *host_a, Chain *dev_a, Config *cfg){
   int m;
   
   for(m = 0; m < cfg->M; ++m){
-    sampleC(host_a, dev_a, cfg); printChain(host_a, dev_a, cfg);
-    sampleTau(host_a, dev_a, cfg); printChain(host_a, dev_a, cfg);
-    samplePiAlp(host_a, dev_a, cfg); printChain(host_a, dev_a, cfg);
-    samplePiDel(host_a, dev_a, cfg); printChain(host_a, dev_a, cfg);
-    sampleD(host_a, dev_a, cfg); printChain(host_a, dev_a, cfg);
-    sampleThePhi(host_a, dev_a, cfg); printChain(host_a, dev_a, cfg);
-    sampleTheAlp(host_a, dev_a, cfg); printChain(host_a, dev_a, cfg);
-    sampleTheDel(host_a, dev_a, cfg); printChain(host_a, dev_a, cfg);
-    sampleSigC(host_a, dev_a, cfg); printChain(host_a, dev_a, cfg);
-    sampleSigPhi(host_a, dev_a, cfg); printChain(host_a, dev_a, cfg);
-    sampleSigAlp(host_a, dev_a, cfg); printChain(host_a, dev_a, cfg);
-    sampleSigDel(host_a, dev_a, cfg); printChain(host_a, dev_a, cfg);
-    sampleEta(host_a, dev_a, cfg); printChain(host_a, dev_a, cfg);
-    sampleEps(host_a, dev_a, cfg); printChain(host_a, dev_a, cfg);
-    samplePhiAlpDel(host_a, dev_a, cfg); printChain(host_a, dev_a, cfg);
+    sampleC(host_a, dev_a, cfg);
+    sampleTau(host_a, dev_a, cfg);
+    samplePiAlp(host_a, dev_a, cfg);
+    samplePiDel(host_a, dev_a, cfg);
+    sampleD(host_a, dev_a, cfg);
+    sampleThePhi(host_a, dev_a, cfg);
+    sampleTheAlp(host_a, dev_a, cfg);
+    sampleTheDel(host_a, dev_a, cfg);
+    sampleSigC(host_a, dev_a, cfg);
+    sampleSigPhi(host_a, dev_a, cfg);
+    sampleSigAlp(host_a, dev_a, cfg);
+    sampleSigDel(host_a, dev_a, cfg);
+    sampleEta(host_a, dev_a, cfg);
+    sampleEps(host_a, dev_a, cfg);
+    samplePhiAlpDel(host_a, dev_a, cfg);
   }
 } 
