96895f367bd08a71f6baa46029f755763044489d
wlandau <will.landau@gmail.com>
2013-07-11 01:00:02 -0500

    "."

--- sh/cputest.sh
+++ sh/cputest.sh
@@ -1,3 +1,4 @@
 make -C .. cpu
+mkdir -p ../out
 
 ../bin/mcmc --data ../data/smallData.txt --group ../data/smallGroup.txt --probs ../out/probs.txt --hyper ../out/hyper.txt --rates ../out/rates.txt --all-parms ../out/allparms.txt --some-parms ../out/someparms.txt
--- sh/gputest.sh
+++ sh/gputest.sh
@@ -1,3 +1,4 @@
 make -C .. gpu
+mkdir -p ../out
 
 ../bin/gpumcmc --data ../data/smallData.txt --group ../data/smallGroup.txt --probs ../out/probs.txt --hyper ../out/hyper.txt --rates ../out/rates.txt --all-parms ../out/allparms.txt --some-parms ../out/someparms.txt
