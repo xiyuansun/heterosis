2a3c01802085232bf163ff59e2312a93a45f4128
wlandau <will.landau@gmail.com>
2013-07-11 01:22:24 -0500

    sampling phi on the gpu

--- src/gpu/phiAlpDel.cu
+++ src/gpu/phiAlpDel.cu
@@ -9,7 +9,7 @@ void samplePhiAlpDel(Chain *host_a, Chain *dev_a, Config *cfg){ /* host */
     samplePhiAlpDelJoint(host_a, dev_a, cfg);
   } else {*/
     samplePhi(host_a, dev_a, cfg);
-  /*  sampleAlp(host_a, dev_a, cfg);
+  /*   sampleAlp(host_a, dev_a, cfg);
     
     if(cfg->heterosis)
       sampleDel(host_a, dev_a, cfg);*/
