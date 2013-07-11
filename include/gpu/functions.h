e27253e5bcf313610fcd134f05dafbf4e408b03c
wlandau <will.landau@gmail.com>
2013-07-10 19:20:24 -0500

    "."

--- include/gpu/functions.h
+++ include/gpu/functions.h
@@ -127,11 +127,9 @@ __global__ void sampleTheAlp_kernel1(Chain*);
 __global__ void sampleTheAlp_kernel2(Chain*);
 __host__ void sampleTheAlp(Chain*, Chain*, Config*);
 
-void sampleTheDel_kernel1(Chain*);
-void sampleTheDel_kernel2(Chain*);
-void sampleTheDel_kernel3(Chain*);
-void sampleTheDel_kernel4(Chain*);
-void sampleTheDel(Chain*, Chain*, Config*);
+__global__ void sampleTheDel_kernel1(Chain*);
+__global__ void sampleTheDel_kernel2(Chain*);
+__host__ void sampleTheDel(Chain*, Chain*, Config*);
 
 void sampleSigPhi_kernel1(Chain*);
 void sampleSigPhi_kernel2(Chain*);
