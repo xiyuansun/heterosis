8d736eb6d075d3162f1799ad3ad92baa8c69e7d6
wlandau <will.landau@gmail.com>
2013-07-11 09:55:34 -0500

    "."

--- src/gpu/newChain.cu
+++ src/gpu/newChain.cu
@@ -15,6 +15,8 @@ __host__ int cmpfunc (const void *a, const void *b){
 __global__ void curand_setup_kernel(Chain *a, unsigned int seed){ /* kernel <<<G, 1>>> */
   int id = ID;
   curand_init(seed, id, 0, &(a->states[id]));
+  
+  a->states[id] = id;
 }
 
 __global__ void newChain_kernel1(Chain *a){ /* kernel <<<G, 1>>> */
