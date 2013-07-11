f72816e0496471560029dacb8b86d8de88443a9d
wlandau <will.landau@gmail.com>
2013-07-11 13:59:17 -0500

    "."

--- src/gpu/oneChain.cu
+++ src/gpu/oneChain.cu
@@ -17,6 +17,7 @@ void oneChain(Config* cfg){
   
   runChain(host_a, dev_a, cfg);
   summarizeChain(host_a, dev_a, cfg);
+  printChain(host_a, dev_a, cfg);
   
   freeChain(host_a, cfg, 0); 
   cudaFree(dev_a);
