d61abd5a5d929f079ad1a2050f9ae774e0087233
wlandau <will.landau@gmail.com>
2013-07-11 18:25:03 -0500

    "."

--- src/gpu/d.cu
+++ src/gpu/d.cu
@@ -118,5 +118,5 @@ __host__ void sampleD(Chain *host_a, Chain *dev_a, Config *cfg){ /* host */
   cudaEventDestroy(start);
   cudaEventDestroy(stop);
   
-  fprintf(cfg->time, "%0.3f ", myTime); /* elapsed time in minutes */
+  fprintf(cfg->time, "---%0.3f ", myTime); /* elapsed time in minutes */
 }
