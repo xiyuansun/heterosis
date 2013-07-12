ed781e1fdfb0d54a9ebfb22f3892924ce04fa30e
wlandau <will.landau@gmail.com>
2013-07-12 18:13:20 -0500

    "."

--- src/gpu/sigAlp.cu
+++ src/gpu/sigAlp.cu
@@ -45,7 +45,7 @@ __host__ void sampleSigAlp(Chain *host_a, Chain *dev_a, Config *cfg){ /* host */
   cudaEventCreate(&start);
   cudaEventCreate(&stop);
   err = cudaEventRecord(start, 0);
-f(err != cudaSuccess) {
+if(err != cudaSuccess) {
           printf ("\n\n 1. Error: %s\n\n", cudaGetErrorString(err));
           exit(1);
         }
