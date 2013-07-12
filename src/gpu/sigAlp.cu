9bedcc4e6a129b4c2fbabf122eb0d7ff3487e026
wlandau <will.landau@gmail.com>
2013-07-12 18:21:40 -0500

    "."

--- src/gpu/sigAlp.cu
+++ src/gpu/sigAlp.cu
@@ -9,9 +9,6 @@
 #include <stdlib.h>
 #include <thrust/reduce.h>
 
-#include "cuPrintf.cuh"
-#include "cuPrintf.cu"
-
 __global__ void sampleSigAlp_kernel1(Chain *a){ /* kernel <<<G, 1>>> */
   int g = IDX, G = a->G;
 
