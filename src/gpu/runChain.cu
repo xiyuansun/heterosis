0943fb581f6ebcb1432cf5e2198590d03698be49
wlandau <will.landau@gmail.com>
2013-07-12 18:53:55 -0500

    "."

--- src/gpu/runChain.cu
+++ src/gpu/runChain.cu
@@ -20,8 +20,8 @@ void runChain(Chain *host_a, Chain *dev_a, Config *cfg){
     sampleTau(host_a, dev_a, cfg);
     samplePiAlp(host_a, dev_a, cfg);
     samplePiDel(host_a, dev_a, cfg); 
- /*   sampleD(host_a, dev_a, cfg); 
-    sampleThePhi(host_a, dev_a, cfg);
+    sampleD(host_a, dev_a, cfg); 
+ /*   sampleThePhi(host_a, dev_a, cfg);
     sampleTheAlp(host_a, dev_a, cfg);
     sampleTheDel(host_a, dev_a, cfg);
     sampleSigC(host_a, dev_a, cfg);
