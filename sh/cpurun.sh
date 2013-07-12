1c704392a05faf4cb503547d5b7fea09c2d196e9
wlandau <will.landau@gmail.com>
2013-07-11 20:39:24 -0500

    scaling up

--- sh/cpurun.sh
+++ sh/cpurun.sh
@@ -1,1 +1,1 @@
-nice -19 nohup ../bin/mcmc --data ../data/data.txt --group ../data/group.txt -r -h -p & -M 1000
+nice -19 nohup ../bin/mcmc --data ../data/data.txt --group ../data/group.txt -r -h -p -M 1000 &
