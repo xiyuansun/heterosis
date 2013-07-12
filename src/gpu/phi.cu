7a4d442afbcf1b3b3db3dee1a6ecfd20eda24a41
wlandau <will.landau@gmail.com>
2013-07-12 18:01:58 -0500

    "."

--- src/gpu/phi.cu
+++ src/gpu/phi.cu
@@ -57,6 +57,8 @@ void samplePhi(Chain *host_a, Chain *dev_a, Config *cfg){ /* host */
   cudaEventCreate(&stop);
   cudaEventRecord(start, 0);
 
+  fprintf(cfg->log, "alp ");
+
   samplePhi_kernel1<<<G_GRID, G_BLOCK>>>(dev_a);
   samplePhi_kernel2<<<1, 1>>>(dev_a);
 
