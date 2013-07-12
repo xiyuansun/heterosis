87b1ec26716f589fb2230613c7262d7cd2eb16ec
wlandau <will.landau@gmail.com>
2013-07-11 20:36:30 -0500

    "."

---
+++ sh/cpurun.sh
@@ -0,0 +1,1 @@
+nice -19 nohup ../bin/mcmc --data ../data/data.txt --group ../data/group.txt -r -h -p & -M 1000
---
+++ sh/gpurun.sh
@@ -0,0 +1,1 @@
+nice -19 nohup ../bin/gpumcmc --data ../data/data.txt --group ../data/group.txt -r -h -p & -M 1000
