7b716dced035f15ce7e94329b9fce905efddd4dc
wlandau <will.landau@gmail.com>
2013-07-12 23:30:18 -0500

    "."

--- sh/cputest.sh
+++ sh/cputest.sh
@@ -1,3 +1,3 @@
 make -C .. cpu
-valgrind --track-origins=yes ../bin/mcmc --data ../data/smallData.txt --group ../data/smallGroup.txt -r -h -p --chains 1 -v
-../bin/mcmc --data ../data/data.txt --group ../data/group.txt -r -h -p --chains 1 -v
+valgrind --track-origins=yes ../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -r -h -p --chains 1 -v
+../bin/mcmc --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt -r -h -p --chains 1 -v
--- sh/gputest.sh
+++ sh/gputest.sh
@@ -1,3 +1,3 @@
 make -C .. gpu
-cuda-memcheck ../bin/gpu-mcmc --data ../data/smallData.txt --group ../data/smallGroup.txt -r -h -p --chains 1 -v
-../bin/gpu-mcmc --data ../data/data.txt --group ../data/group.txt -r -h -p --chains 1 -v
+cuda-memcheck ../bin/gpu-mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -r -h -p --chains 1 -v
+../bin/gpu-mcmc --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt -r -h -p --chains 1 -v
