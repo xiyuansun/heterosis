1a070c0155344492b3748ed416bc831ef98a9c44
wlandau <will.landau@gmail.com>
2013-07-13 09:20:11 -0500

    "."

--- sh/gputest.sh
+++ sh/gputest.sh
@@ -1,6 +1,6 @@
 git pull origin master
 make -C .. gpu
 
-#cuda-memcheck ../bin/gpu-mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -r -h -p --chains 1 -v -t
+cuda-memcheck ../bin/gpu-mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -r -h -p --chains 1 -v -t
 
-../bin/gpu-mcmc --data ../data/test/mediumData.txt --group ../data/test/mediumGroup.txt -r -h -p --chains 1 -v -t -M 100
+#../bin/gpu-mcmc --data ../data/test/mediumData.txt --group ../data/test/mediumGroup.txt -r -h -p --chains 1 -v -t -M 100
