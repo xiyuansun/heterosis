7a728cd6f083a29512a9d915a4a2e0cb9667f357
wlandau <will.landau@gmail.com>
2013-07-12 20:04:53 -0500

    "."

--- src/gpu/alp.cu
+++ src/gpu/alp.cu
@@ -10,7 +10,7 @@
 
 __device__ num_t lAlp(Chain *a, int g, num_t arg){ /* device */
   
-  int n, N = a->N, G = a->G;
+/*  int n, N = a->N, G = a->G;
   num_t s = 0, tmp;
    
   for(n = 0; n < a->N; ++n){
@@ -28,7 +28,8 @@ __device__ num_t lAlp(Chain *a, int g, num_t arg){ /* device */
     tmp = log(a->piAlp[a->mPiAlp]);
   }
 
-  return s + tmp;
+  return s + tmp; */
+  return 1;
 }
 
 __global__ void sampleAlp_kernel1(Chain *a){ /* kernel <<<G, 1>>> */
@@ -40,7 +41,7 @@ __global__ void sampleAlp_kernel1(Chain *a){ /* kernel <<<G, 1>>> */
     old = a->alp[iG(a->mAlp, g)];
     nw = alpProp(a, g);
     
-    dl = -1e12; /* lAlp(a, g, nw) - lAlp(a, g, old); */
+    dl = lAlp(a, g, nw) - lAlp(a, g, old); 
     lp = 0 < dl ? 0 : dl;
     lu = log(runiformDevice(a, g, 0, 1));
     
