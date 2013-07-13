353483ad92eb273695df4485fec18310f8f50877
wlandau <will.landau@gmail.com>
2013-07-12 23:20:30 -0500

    "."

--- sh/cputest.sh
+++ sh/cputest.sh
@@ -1,2 +1,2 @@
 make -C .. cpu
-valgrind --track-origins=yes ../bin/mcmc --data ../data/smallData.txt --group ../data/smallGroup.txt -r -h -p --chains 1 -v
+valgrind --track-origins=yes ../bin/mcmc --data ../data/mediumData.txt --group ../data/mediumGroup.txt -r -h -p --chains 1 -v
