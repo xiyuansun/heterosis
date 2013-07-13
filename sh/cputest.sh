affc46370aa2f0673773f2509d712bdbaa283af6
wlandau <will.landau@gmail.com>
2013-07-13 03:11:14 -0500

    "."

--- sh/cputest.sh
+++ sh/cputest.sh
@@ -1,3 +1,5 @@
 make -C .. cpu
-valgrind --track-origins=yes ../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -r -h -p --chains 1 -v
-../bin/mcmc --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt -r -h -p --chains 1 -v
+
+#valgrind --track-origins=yes ../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -r -h -p --chains 1 -v -t
+
+../bin/mcmc --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt -r -h -p --chains 1 -v -t
--- sh/gputest.sh
+++ sh/gputest.sh
@@ -1,3 +1,5 @@
 make -C .. gpu
-cuda-memcheck ../bin/gpu-mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -r -h -p --chains 1 -v
-../bin/gpu-mcmc --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt -r -h -p --chains 1 -v
+
+#cuda-memcheck ../bin/gpu-mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -r -h -p --chains 1 -v -t
+
+../bin/gpu-mcmc --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt -r -h -p --chains 1 -v -t
