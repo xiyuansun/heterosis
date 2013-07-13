7e02b24681b0472e04692e64acd820f9468c5857
wlandau <will.landau@gmail.com>
2013-07-12 19:17:02 -0500

    "."

--- src/gpu/runChain.cu
+++ src/gpu/runChain.cu
@@ -15,14 +15,14 @@ void runChain(Chain *host_a, Chain *dev_a, Config *cfg){
   for(m = 0; m < cfg->M; ++m){
 
     fprintf(cfg->log, "    iter %d | ", m);
-
+/*
     sampleC(host_a, dev_a, cfg); 
     sampleTau(host_a, dev_a, cfg);
     samplePiAlp(host_a, dev_a, cfg);
     samplePiDel(host_a, dev_a, cfg); 
     sampleD(host_a, dev_a, cfg); 
     sampleThePhi(host_a, dev_a, cfg);
- /*   sampleTheAlp(host_a, dev_a, cfg); */
+    sampleTheAlp(host_a, dev_a, cfg); 
     sampleTheDel(host_a, dev_a, cfg);
     sampleSigC(host_a, dev_a, cfg);
     sampleSigPhi(host_a, dev_a, cfg);
@@ -30,7 +30,7 @@ void runChain(Chain *host_a, Chain *dev_a, Config *cfg){
     sampleSigDel(host_a, dev_a, cfg); 
     sampleEta(host_a, dev_a, cfg);  
     sampleEps(host_a, dev_a, cfg);
-    samplePhiAlpDel(host_a, dev_a, cfg); 
+    samplePhiAlpDel(host_a, dev_a, cfg); */
 
     fprintf(cfg->time, "\n");
     fprintf(cfg->log, "\n");
