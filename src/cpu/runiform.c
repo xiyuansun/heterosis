958b91514f428aac9aaeca818117031673e04f95
wlandau <will.landau@gmail.com>
2013-07-13 07:21:14 -0500

    "."

--- src/cpu/runiform.c
+++ src/cpu/runiform.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 
 num_t runiform(num_t lb, num_t ub){
-  int max = 1e5;
+ /* int max = 1e5;
   int i = rand() % max;
   num_t maxf = (num_t) max;
   num_t u = (num_t) i;
   
   u /= maxf;
-  u = (ub - lb) * u + lb;
-  return u;
+  u = (ub - lb) * u + lb;*/
+  return 0.5;
 }
