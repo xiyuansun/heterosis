0f67b77cf789d9c5926c966d93b32108890bbfd1
wlandau <will.landau@gmail.com>
2013-07-13 22:02:23 -0500

    "."

--- sh/gputest.sh
+++ sh/gputest.sh
@@ -2,4 +2,4 @@ make -C .. gpu
 
 #cuda-memcheck ../bin/gpu-mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -r -h -p --chains 1 -v -t
 
-../bin/gpu-mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -r -h -p --chains 3 -v -t -M 10
+../bin/gpu-mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -r -h -p --chains 3 -t -M 10
