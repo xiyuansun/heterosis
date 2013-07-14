cdb5e28c15a46fcb17c438e9c0a7ef2343dfaab6
wlandau <will.landau@gmail.com>
2013-07-13 23:24:47 -0500

    "."

--- sh/cputest.sh
+++ sh/cputest.sh
@@ -2,4 +2,4 @@ make -C .. cpu
 
 #valgrind ../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt  -h -p --chains 1 -r -v 
 
-../bin/mcmc --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt -p --chains 1 -h -r -t -M 10000 -v
+../bin/mcmc --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt -p --chains 1 -h -r -t -M 100 -v
