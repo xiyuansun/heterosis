6261ea9b992579c07dc5534412dd92e2da90fc6a
wlandau <will.landau@gmail.com>
2013-07-14 17:15:43 -0500

    stable cpu version, passed valgrind checks

--- sh/cputest.sh
+++ sh/cputest.sh
@@ -4,6 +4,4 @@ make -C .. cpu
 
 #valgrind ../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -h -p --chains 3 -r -b 5 
 
-valgrind ../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt --chains 3 -M 10 --diagnostics --tau 4
-
-#valgrind --track-origins=yes ../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt --chains 3 -M 10 --diagnostics 
+../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt --chains 3 -M 10 --diagnostics 
