b7b943f47cdac41a85ddb91f18c7d88a514ba0ff
wlandau <will.landau@gmail.com>
2013-07-14 12:29:04 -0500

    cpu version passed valgrind checks with DIC

--- sh/cputest.sh
+++ sh/cputest.sh
@@ -1,7 +1,7 @@
 make -C .. cpu
 
-valgrind ../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -h -p --chains 3 -r -b 5 --diagnostics
+#valgrind ../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -h -p --chains 3 -r -b 5 --diagnostics
 
-valgrind ../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -h -p --chains 3 -r -b 5 
+#valgrind ../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -h -p --chains 3 -r -b 5 
 
-#../bin/mcmc --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt --chains 1 -M 100 -v -r -h -t
+../bin/mcmc --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt --chains 1 -M 100 -v -r -h -t --diagnostics
