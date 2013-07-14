a15ef2c854a008f904abad98a6d35158e037fdb8
wlandau <will.landau@gmail.com>
2013-07-14 14:34:15 -0500

    "."

--- src/gpu/allocConfig.cu
+++ src/gpu/allocConfig.cu
@@ -9,6 +9,6 @@ __host__ void allocConfig(Config *cfg){
   cfg->yMeanG = (num_t*) malloc(cfg->N * sizeof(num_t));
 
   CUDA_CALL(cudaMalloc((void**) &(cfg->devY), cfg->N * cfg->G * sizeof(count_t)));
-  CUDA_CALL(cudaMalloc((void**) &(cfg->devGroup), cfg->N * sizeof(count_t)));
+  CUDA_CALL(cudaMalloc((void**) &(cfg->devGrp), cfg->N * sizeof(count_t)));
   CUDA_CALL(cudaMalloc((void**) &(cfg->devYMeanG), cfg->N * sizeof(count_t)));
 }
