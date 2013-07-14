e0e25aaa0ecdc6156c463767e9b4ab380a733a2b
wlandau <will.landau@gmail.com>
2013-07-14 15:48:17 -0500

    "."

--- src/gpu/resetChain.cu
+++ src/gpu/resetChain.cu
@@ -6,6 +6,6 @@
 #include <stdio.h>
 
 __host__ void resetChain(Chain *host_a, Chain *dev_a, Config *cfg){
-  newChain_kernel1<<<G_GRID, G_BLOCK>>>(*dev_a);
-  newChain_kernel2<<<1, 1>>>(*dev_a);
+  newChain_kernel1<<<G_GRID, G_BLOCK>>>(dev_a);
+  newChain_kernel2<<<1, 1>>>(dev_a);
 }
