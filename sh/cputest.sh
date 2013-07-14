32e833953a3a3eaeb408b64b202f4ebf6f55d9c8
wlandau <will.landau@gmail.com>
2013-07-13 23:37:03 -0500

    if output to files is minimal, the gpu version can run 100 iterations on a dataset of 35000+ genes and 16 libraries in just over 10 seconds. the cpu version in this case took around 50 seconds. if all parameters are sent to output, then the gpu version takes 30 seconds and the cpu version takes about a minute and 15 seconds in this scenario.

--- sh/cputest.sh
+++ sh/cputest.sh
@@ -2,4 +2,4 @@ make -C .. cpu
 
 #valgrind ../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt  -h -p --chains 1 -r -v 
 
-../bin/mcmc --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt --chains 1 -M 100 -v
+../bin/mcmc --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt --chains 1 -M 100 -v -r -h -t
