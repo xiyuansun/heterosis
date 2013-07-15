09403595c74efd555e9d1e072b782493afa77d62
wlandau <will.landau@gmail.com>
2013-07-14 19:05:52 -0500

    "."

--- sh/cputest.sh
+++ sh/cputest.sh
@@ -4,4 +4,4 @@ make -C .. cpu
 
 #valgrind ../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -h -p --chains 3 -r -b 5 
 
-valgrind ../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt --chains 1 -M 10 --diagnostics 
+../bin/gpu-mcmc --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt -M 100 -c 1 --diagnostics -v
