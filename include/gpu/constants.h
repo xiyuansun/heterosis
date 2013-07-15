be908ee119a612da0ce1af0f0a026467e3c5e23e
wlandau <will.landau@gmail.com>
2013-07-15 16:34:36 -0500

    "."

--- include/gpu/constants.h
+++ include/gpu/constants.h
@@ -3,11 +3,11 @@
 
 #define BUF 256
 #define MAXROW 16384
-#define NUM_TF "%0.3f"
 #define NUM_TMIN (-1.0/0.0)
 #define MAX_NG ((N > G) ? N : G)
 
 #define MAXTHREADS 512
+#define IDX ((blockDim.x * blockIdx.x) + threadIdx.x)
 
 #define N_BLOCK (cfg->N < MAXTHREADS ? cfg->N : MAXTHREADS)
 #define N_GRID ((cfg->N / N_BLOCK) + 1)
@@ -18,11 +18,10 @@
 #define NG_BLOCK (MAX_NG < MAXTHREADS ? MAX_NG : MAXTHREADS)
 #define NG_GRID ((MAX_NG / NG_BLOCK) + 1)
 
-#define IDX ((blockDim.x * blockIdx.x) + threadIdx.x)
-
 #define MILLISECS 1.0 /* number of milliseconds in a unit of time in output */
 
 typedef int count_t;
-typedef float num_t;
+typedef double num_t;
+#define NUM_TF "%0.3f"
 
 #endif /* CONSTANTS_H */
