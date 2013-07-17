8249fd35dc2ccbdeb93178996c9e4e04d05d96fa
wlandau <will.landau@gmail.com>
2013-07-17 16:23:50 -0500

    "."

--- sh/gputest.sh
+++ sh/gputest.sh
@@ -1,3 +1,3 @@
 make -C .. gpu
 
-../bin/gpu-mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroupDe.txt -M 20 -c 3 -h -p -P  -r -t --dic 
+../bin/gpu-mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -M 20 -c 3 -h -p -P  -r -t --dic 
