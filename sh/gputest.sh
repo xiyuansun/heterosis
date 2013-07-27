2f16e8bf40629ed74b99e1edb6c1c4c2d05fd290
wlandau <landau@iastate.edu>
2013-07-27 02:18:11 -0500

    plugging in options for priors to main code in gpu version

--- sh/gputest.sh
+++ sh/gputest.sh
@@ -1,3 +1,3 @@
 make -C .. gpu
 
-../bin/gpu-mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -M 3 -c 2 -h -p -P  -r -t --dic --phi-prior 4 --alpha-prior 5 --delta-prior 6
+../bin/gpu-mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -h -p -P --dic -r -t -j -o out2j
