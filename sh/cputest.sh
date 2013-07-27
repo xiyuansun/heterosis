5522b0fe7a607f30bae2e3c163ddba982f94f035
wlandau <will.landau@gmail.com>
2013-07-27 01:26:09 -0500

    "."

--- sh/cputest.sh
+++ sh/cputest.sh
@@ -1,3 +1,3 @@
 make -C .. cpu
 
-../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -M 3 -c 2 -h -p -P  -r -t --dic 
+../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -M 3 -c 2 -h -p -P  -r -t --dic -o /lksj
