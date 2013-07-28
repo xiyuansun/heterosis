84ef6e4f5f654c466d8ad397575739bad3fd016a
wlandau <will.landau@gmail.com>
2013-07-28 14:53:09 -0500

    "."

--- R/mcmc.r
+++ R/mcmc.r
@@ -1,3 +1,7 @@
 heterosis_mcmc = function(){
   dyn.load(paste(.libPaths(), "/heterosis/libs/heterosis.so"))
 }
+
+gpu_heterosis_mcmc = function(){
+  dyn.load(paste(.libPaths(), "/heterosis/libs/heterosis.so"))
+}
