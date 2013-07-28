be7de22cfa38a9ce61910d2767edd2b8bda2450e
wlandau <will.landau@gmail.com>
2013-07-28 16:54:41 -0500

    "."

--- sh/cputest.sh
+++
@@ -1,3 +0,0 @@
-make -C .. cpu
-
-../bin/mcmc --data ../data/test/smallData.txt --group ../data/test/smallGroup.txt -M 3 -c 2 -h -p -P  -r -t --dic -j -v -j
--- sh/gputest.sh
+++
@@ -1,3 +0,0 @@
-make -C .. gpu
-
-../bin/gpu-mcmc --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt -h -p -r -t -v -M 1000 -c 2
