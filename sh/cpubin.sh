c862d8f217bf7f469c1a5c55064484180882b83d
wlandau <will.landau@gmail.com>
2013-09-29 13:11:21 -0500

    "."

---
+++ sh/cpubin.sh
@@ -0,0 +1,1 @@
+../bin/cpubin --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt --iter 2000 --chains 1 --verbose --hyper --probs
---
+++ sh/gpubin.sh
@@ -0,0 +1,1 @@
+../bin/gpubin --data ../data/test/largeData.txt --group ../data/test/largeGroup.txt --iter 2000 --chains 1 --verbose --hyper --probs
