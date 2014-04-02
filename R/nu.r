d037a7b26310641a5d931726b0747f5fffa202e5
wlandau <will.landau@gmail.com>
2014-04-02 13:57:05 -0500

    working on better metropolis proposals, including a more efficient and sensible way to sample nu

--- R/nu.r
+++ R/nu.r
@@ -5,7 +5,6 @@ G = 10000
 gam = 1/sqrt(rgamma(G, shape = nu/2, rate = nu * tau^2/2))
 
 lp = function(nu){
- # nu[nu <= 0] = 0.0001
   - lgamma(nu/2) + nu/2 * log(nu*tau^2/2) - nu* mean(log(gam) + tau^2/2 * 1/gam^2)
 }
 
@@ -13,46 +12,15 @@ p = function(nu){
   exp(lp(nu))
 }
 
-wd = 0.5
-bd = 10
+# insert new gamma approximation here
 
-f = Vectorize(function(x){
-  i = x / wd
-  if(i <= 1)
-    p(wd)
-  else
-    max(p(floor(i) * wd), p(ceiling(i) * wd))
-}, "x")
-
-ys = f(1:(bd / wd)*wd + wd/2)
-cy = cumsum(ys)
-sampleF = function(){
-  u = runif(1, 0, max(cy))
-
-  for(i in length(ys):1)
-    if(cy[i] < u)
-      break
-
-  m = min(ys[i], ys[i + 1])  
-  M = max(ys[i], ys[i + 1])
-
-  (runif(1) + i - 1) * wd 
+f = function(nu){
+  dgamma(nu, shape = 3/2, rate = -1/2 - log(tau^2)/2 + mean(log(gam) + tau^2/2 * 1/gam^2))
 }
 
-sampleFs = function(n){
-  ret = c()
-  for(i in 1:n)
-    ret[i] = sampleF()
-  ret
-}
-
-ss = sampleFs(10000)
-
-hist(ss,  freq = F, col = "red")
-curve(f, 0.001, 10, col = "blue", add = T, lwd = 2)
+curve(f, 0.001, 10, col = "blue", lwd = 2)
 xsp = 1:10000/1000
 ysp = p(xsp)
 lines(x =xsp, y = ysp, lwd = 2)
 
-legend("topright", fill = c("black", "blue"), legend = c("Const * exp(h(nu))", "Approximation"))
-
+legend("topright", legend = c("full conditional", "approximation"), fill = c("black", "blue"))
