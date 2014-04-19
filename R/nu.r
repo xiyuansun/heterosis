e6fc3ee7457af8b31de8955d0eb1a3f32db8fdcd
wlandau <will.landau@gmail.com>
2014-04-19 18:20:11 -0500

    tweaked the samplers based on last weeks meeting

--- R/nu.r
+++ R/nu.r
@@ -1,4 +1,5 @@
 d = 1000
+u = 8
 tau = 1
 nu = 1
 G = 10000
@@ -13,22 +14,31 @@ up = function(nu){
 }
 
 library(pracma)
-x = seq(0.01, 10*d, by = .1)
+x = seq(0.001, d, by = .01)
 area = trapz(x = x, up(x))
 
 p = function(x){
   up(x) / area
 }
 
+#md = x[which.max(lp(x))]
+rate = -1/2 - log(tau^2)/2 + mean(log(gam) + tau^2/2 * 1/gam^2) 
+shape = 3/2 #md * rate + 1
+
+rate = rate * 0.99
+
+mn = shape/rate
+v = shape/rate^2
+
 f = function(nu){
-  dgamma(nu, shape = 3/2, rate = -1/2 - log(tau^2)/2 + mean(log(gam) + tau^2/2 * 1/gam^2))
+  dgamma(nu, shape = shape, rate = rate)
 }
 
-curve(f, 0.001, 10, col = "blue", lwd = 2)
-xsp = 1:(10*d)/10
-ysp = p(xsp)
-lines(x =xsp, y = ysp, lwd = 2)
+curve(p, 0.001, u, col = "black", lwd = 2)
+xsp = seq(0.001, u, by = .01)
+ysp = f(xsp)
+lines(x =xsp, y = ysp, lwd = 2, col = "blue")
 
 legend("topright", legend = c("full conditional", "approximation"), fill = c("black", "blue"))
 
-# x0 = 1000; f(x0)/p(x0)
+x0 = 1000; f(x0)/p(x0)
